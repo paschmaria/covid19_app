bad726730d73979c76ca6f924302076d7c8c34ee
Paschal Onuorah <onuorahpaschal@gmail.com>
2020-04-02 11:53:26 +0100

    Whitenoise setup for staticfiles storage

--- core/settings/base.py
+++ core/settings/base.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+                os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            )
 
 
 # Quick-start development settings - unsuitable for production
@@ -44,6 +46,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # django-simple-history
+    'simple_history.middleware.HistoryRequestMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -107,11 +111,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Specify format for phone numbers used in account creation
+PHONENUMBER_DB_FORMAT = 'INTERNATIONAL'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Emails
